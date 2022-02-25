54279992221d87e93a4a3cfc392368a381b98899
ankitbhusal <ankitbhusal95@gmail.com>
2022-02-25 08:44:41 +0545

    db

--- databse_files/mtbs.sql
+++ databse_files/mtbs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2022 at 04:18 PM
+-- Generation Time: Feb 25, 2022 at 03:59 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,6 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `show_id` int(11) NOT NULL,
+  `total_tickets` int(11) NOT NULL,
+  `unit_price` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`id`, `name`, `description`) VALUES
+(1, 'Horror', 'Zombies, serial killers, or ghosts'),
+(2, 'Sci-Fi', 'Outer space or futuristic items like spaceships or laser guns'),
+(5, 'War', ' Different vehicles such as tanks, planes, or realistic battlefields'),
+(11, 'Action', 'Chase sequences, extended fight scenes, guns, races against time'),
+(12, 'Romance', 'Different stages of “falling in love” with a subsequent break-up and reconciliation, true love, fairy tales, forbidden love'),
+(13, 'Comedy', 'Slapstick humor, witty dialogue, rites of passage, gross-out humor, fish-out-of-water, cross-dressing, mistaken identity'),
+(14, 'Crime', 'Whodunnits, capers, rival gangs, robberies'),
+(15, 'Drama', 'Narrative fiction intended to be more serious than humorous in tone'),
+(16, 'Thriller', 'Thriller is a genre of fiction, having numerous, often overlapping subgenres. Thrillers are characterized and defined by the moods they elicit, giving viewers heightened feelings of suspense, excitement, surprise, anticipation and anxiety');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `genre_movie`
 --
 
@@ -34,8 +76,134 @@ CREATE TABLE `genre_movie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `genre_movie`
+--
+
+INSERT INTO `genre_movie` (`id`, `genre_id`, `movie_id`) VALUES
+(3, 11, 16),
+(5, 15, 16);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hall`
+--
+
+CREATE TABLE `hall` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `total_seats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hall`
+--
+
+INSERT INTO `hall` (`id`, `name`, `total_seats`) VALUES
+(18, 'hello2', 50),
+(22, 'new hall', 50),
+(23, 'CinemaTIC', 40),
+(24, 'cnema', 23),
+(25, 'nephall', 32),
+(26, 'asdad', 25),
+(28, 'test', 27),
+(31, 'testst3', 34),
+(32, 'ankit', 23),
+(33, 'ankit2', 45);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+CREATE TABLE `movie` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `language` varchar(10) NOT NULL,
+  `release_date` date NOT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `genre` varchar(20) NOT NULL,
+  `hh` varchar(1) DEFAULT NULL,
+  `mm` varchar(2) DEFAULT NULL,
+  `ss` varchar(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie`
+--
+
+INSERT INTO `movie` (`id`, `name`, `language`, `release_date`, `image`, `genre`, `hh`, `mm`, `ss`) VALUES
+(15, 'The Shawshank Redemption', 'English', '1994-10-14', '6217a0e73004a.jpeg', 'Drama', '2', '22', '0'),
+(16, 'The Godfather', 'English', '1972-03-24', '6217be77b02f7.jpeg', 'Crime,Drama', '2', '55', '0'),
+(17, 'The Dark Knight', 'English', '2008-07-18', '6217c038d344e.jpeg', 'Action,Crime,Drama', '2', '32', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL,
+  `booking_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `payment_on` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `show`
+--
+
+CREATE TABLE `show` (
+  `id` int(11) NOT NULL,
+  `hall_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `play_date` date NOT NULL,
+  `play_time` time NOT NULL,
+  `ticket_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` varchar(5) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'ankit', 'ankit@ankit.com', '$2y$10$VfUe9A5HQdgr.xkHG5ZdSONk58cjBM0fGiaOt1oKiErJWMMU.dIeG', 'user'),
+(2, 'admin', 'admin@admin.com', '$2y$10$Ly2dVfzg91Y8Bicq4GX.qevdi7nOkg2cOrw36ZV4lrWA/oJ64aMnu', 'admin'),
+(3, 'user', 'user@user.com', '$2y$10$t7bUX.E4rUULdwNHOq3Di.ynhGioa7CafUinTXDXcK5FWy47pvGKm', 'user');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `genre_movie`
@@ -46,14 +214,86 @@ ALTER TABLE `genre_movie`
   ADD KEY `movie_id` (`movie_id`);
 
 --
+-- Indexes for table `hall`
+--
+ALTER TABLE `hall`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `show`
+--
+ALTER TABLE `show`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `genre_movie`
 --
 ALTER TABLE `genre_movie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `hall`
+--
+ALTER TABLE `hall`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `show`
+--
+ALTER TABLE `show`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
