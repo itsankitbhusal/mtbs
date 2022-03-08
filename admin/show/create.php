34356999baf287f9e3ad7d3988ae437fa49e77c7
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-08 10:42:59 +0545

    show crud start

---
+++ admin/show/create.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once __DIR__ . "/../components/admin.php";
+
+
+
+include  __DIR__ . "/../components/header.php";
+include  __DIR__ . "/../components/sidebar.php";
+
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-flex m-4 justify-content-between mb-4">
+        <h3 class="font-weight-bold">Add Show</h3>
+        <a href="./index.php" class="btn btn-primary px-4 font-weight-bold">Go back</a>
+    </div>
+    <?php if (hasError()) : ?>
+        <div class="ml-4 alert alert-danger">
+            <?php echo getError(); ?>
+        </div>
+    <?php endif; ?>
+    <?php if (hasSuccess()) : ?>
+        <div class="ml-4 alert alert-success">
+            <?php echo getSuccess(); ?>
+        </div>
+    <?php endif; ?>
+    <!-- /.container-fluid -->
+    <form class="m-4" method="POST" action="./store.php" enctype="multipart/form-data">
+
+
+        <div class="form-group">
+
+            <label for="language">Hall</label>
+            <select name="hall_id" class="form-control" id="language">
+                <?php $hall = all('hall');
+                foreach ($hall as $h) :
+                ?>
+                    <option value="<?php echo $h['id']; ?>"><?php echo $h['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="form-group">
+
+            <label for="language">Movie</label>
+            <select name="movie_id" class="form-control" id="language">
+                <?php $movie = all('movie');
+                foreach ($movie as $m) :
+                ?>
+                    <option value="<?php echo $m['id']; ?>"><?php echo $m['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="releseDate">Play Date</label>
+            <input name="play_date" type="date" value="<?php echo date('Y-m-d'); ?>" class="form-control" id="releseDate">
+        </div>
+        <div class="form-group">
+            <label for="releseDate">Play Time</label>
+            <input name="play_time" type="time" value="<?php date_default_timezone_set('Asia/Kathmandu');
+                                                        echo date('h:i'); ?>" class="form-control" id="releseDate">
+        </div>
+
+        <div class="form-group">
+            <label for="releseDate">Ticket Price</label>
+            <input name="ticket_price" type="number" class="form-control" id="releseDate">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Add movie</button>
+
+    </form>
+</div>
+</div>
+<!-- End of Main Content -->
+<?php include __DIR__ . "/../components/footer.php"; ?>
---
+++ admin/show/store.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__ . "/../components/admin.php";
+echo '<pre>';
+print_r($_POST);
+die;
+
+if (empty($_POST['hall_id']) || empty($_POST['movie_id']) || empty($_POST['play_date']) || empty($_POST['play_time']) || empty($_POST['ticket_price'])) {
+    setError('Please fill all the fields!!');
+    Header("Location: ./create.php");
+}
+
+if (!empty($_POST)) {
+    $hall_id = request('hall_id');
+    $movie_id = request('movie_id');
+    $play_date = request('play_date');
+    $play_time = request('play_time');
+    $ticket_price = request('ticket_price');
+
+
+    if (!empty($hall_id) && !empty($movie_id) && !empty($play_date) && !empty($play_time) && !empty($ticket_price)) {
+
+
+        create('show', [
+            'hall_id' => $hall_id,
+            'movie_id' => $movie_id,
+            'play_date' => $play_date,
+            'play_time' => $play_time,
+            'ticket_price' => $ticket_price
+        ]);
+
+        setSuccess('Data Inserted Sucessfully');
+        header("Location: index.php");
+    } else {
+        setError("Please fill all the fields");
+        Header("Location: create.php");
+    }
+}
