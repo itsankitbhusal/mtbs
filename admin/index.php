3e156c0712b47c95e3c4b14abecbd33fc9495ec8
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-14 18:11:45 +0545

    added message

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,17 @@ include_once __DIR__ . "/./components/sidebar.php";
 <div class="container-fluid">
   <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800">Admin Dashboard</h1>
+  <?php if (hasError()) : ?>
+    <div id="error" class="ml-4 alert alert-danger">
+      <?php echo getError(); ?>
+    </div>
+  <?php endif; ?>
+  <?php if (hasSuccess()) : ?>
+    <div id="success" class="ml-4 alert alert-success">
+      <?php echo getSuccess(); ?>
+    </div>
+  <?php endif; ?>
+
 
 </div>
 <!-- /.container-fluid -->
