046e1a5c26ac138b473f04b014c909f51dc4c0f0
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-17 12:47:20 +0545

    fixed null error

--- admin/js/script.js
+++ admin/js/script.js
@@ -3,14 +3,19 @@ function confirmDelete(id) {
     location.href = `./delete.php?id=${id}`;
   }
 }
-setTimeout(() => {
-  const error = document.getElementById("error");
-  error.classList.add("d-none");
-  error.classList.add("fade");
-}, 3000);
-
-setTimeout(() => {
-  const success = document.getElementById("success");
-  success.classList.add("d-none");
-  success.classList.add("fade");
-}, 3000);
+const error = document.getElementById("error");
+if (error) {
+  setTimeout(() => {
+    // const error = document.getElementById("error");
+    error.classList.add("d-none");
+    error.classList.add("fade");
+  }, 3000);
+}
+const success = document.getElementById("success");
+if (success) {
+  setTimeout(() => {
+    // const success = document.getElementById("success");
+    success.classList.add("d-none");
+    success.classList.add("fade");
+  }, 3000);
+}
