0acd0c9f53354ccc09c54ac5991f37d495d638c1
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-21 19:50:43 +0545

    automated shows deletion

--- admin/components/admin.php
+++ admin/components/admin.php
@@ -4,3 +4,27 @@ include  __DIR__ . "/../../functions/functions.php";
 
 $result = check_admin();
 $page_url = "http://localhost/mtbs/admin/";
+
+$shows = all('shows');
+echo "<pre>";
+print_r($shows);
+
+foreach ($shows as $s) {
+    $date = $s['play_date'];
+    $currentDate = date("Y-m-d");
+
+    if ($date < $currentDate) {
+
+        delete('shows', $s['id']);
+    }
+}
+foreach ($shows as $s) {
+    $time = $s['play_time'];
+    if (!empty($time)) {
+        $current_timestamp  = time();
+        $current_time = date('H:i:s', $current_timestamp);
+        if ($time < $current_time) {
+            delete('shows', $s['id']);
+        }
+    }
+}
