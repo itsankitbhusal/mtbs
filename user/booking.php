c23ef63db9bf549146fe44bd28d245278ad62b32
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-22 21:11:09 +0545

    minor

--- user/booking.php
+++ user/booking.php
@@ -3,8 +3,6 @@ require "./components/user.php";
 
 check_user();
 
-
-
 //user id 
 $user_id = $_SESSION['user_id'];
 
@@ -12,7 +10,7 @@ $user_id = $_SESSION['user_id'];
 //show id from ticket GET method
 $show_id = request('sid');
 //hall id form ticket GET method
-$hall_id = request('hid');
+// $hall_id = request('hid');
 
 //total seats
 $total_tickets = request('tt');
@@ -21,23 +19,9 @@ $total_tickets = request('tt');
 $unit_price = request('price');
 
 $booked_seat = request('booked_seats');
-
-
-$booking_total = query("SELECT SUM(booked_seat) FROM booking WHERE status='booked'"); //5
-$total_seats = query('SELECT `total_seats` FROM `hall` WHERE id =' . $hall_id, false); //50
-
-$updated_seats = (int)$booking_total + (int)$booked_seat;
-
-if ($total_seats <= $updated_seats) {
-    setError('Maximum seat limit reached!!');
-    $url = './book.php/id=' . $id;
-    header("Location: " . $url);
-    die;
-}
-
 if ($booked_seat > 5) {
     setError("You cannot book more than 5 seats!!");
-    $url = './ticket.php';
+    $url = './ticket.php?id=' . $show_id;
     header('Location: ' . $url);
     die;
 }
