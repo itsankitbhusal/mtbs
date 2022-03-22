a23fad65d6d8f2d4fce20b57cecf47771149964f
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-22 20:47:45 +0545

    working regex

--- functions/validation.php
+++ functions/validation.php
@@ -9,10 +9,7 @@
  */
 function validateName($name)
 {
-    if (preg_match("/^[a-zA-Z'. -]+$/", $name)) {
-        return true;
-    }
-    return false;
+    return preg_match("/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/", $name);
 };
 
 /**
@@ -25,11 +22,7 @@ function validateName($name)
 
 function validatePhone($phone)
 {
-    if (preg_match('(?:\+977[- ])?\d{2}-?\d{7,8}', $phone)) {
-        return true;
-    } else {
-        return false;
-    }
+    return preg_match('/^\+?(?:977)?[ -]?(?:(?:(?:98|97)-?\d{8})|(?:01-?\d{7}))$/', $phone);
 };
 
 /**
@@ -46,9 +39,14 @@ function validateNumber($value)
 }
 
 /**
- * Validate expiry date of card
+ * Validate Strong Password
  *
- * @param string $value date
+ * @param string $password string
  *
  * @return true
  */
+
+function validatePassword($password)
+{
+    return preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/', $password);
+}
