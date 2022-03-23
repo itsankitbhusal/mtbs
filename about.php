b75553d679c04b7e83e40e4c9d0cde63f34f5f08
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-23 09:43:43 +0545

    about page

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php require_once "./user/components/header.php"; ?>
+<div class="image-aboutus-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="lg-text font-weight-bold ">About</h1>
+                <p class="image-aboutus-para">About section about CinemaTic</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container my-5">
+    <div class="row">
+        <div class="mt-5">
+            <h1 class="strong">Who we are and what we do</h1>
+        </div>
+        <div class=" my-5">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam. Lorem ipsum dolor sit amet. Nulla convallis egestas rhoncus.</p>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+<?php require_once "./user/components/footer.php"; ?>
