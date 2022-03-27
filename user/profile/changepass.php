5a24aad5216a604c097d37df93da0ef0cde53a00
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-27 20:44:54 +0545

    change pass

--- user/profile/changepass.php
+++ user/profile/changepass.php
@@ -1,6 +1,16 @@
 <?php require_once "./components/header.php";
 require_once "./components/user.php";
 ?>
+<?php if (hasError()) : ?>
+    <div id="error" class="ml-4 alert alert-danger">
+        <?php echo getError(); ?>
+    </div>
+<?php endif; ?>
+<?php if (hasSuccess()) : ?>
+    <div id="success" class="ml-4 alert alert-success">
+        <?php echo getSuccess(); ?>
+    </div>
+<?php endif; ?>
 <div class="card overflow-hidden">
     <div class="row no-gutters row-bordered row-border-light">
         <div class="col-md-3 pt-0">
@@ -11,7 +21,7 @@ require_once "./components/user.php";
             </div>
         </div>
         <div class="col-md-9">
-            <div class="tab-content ">
+            <form method="POST" action="./changepass.inc.php" class="tab-content ">
 
 
                 <div class="">
@@ -19,27 +29,26 @@ require_once "./components/user.php";
 
                         <div class="form-group">
                             <label class="form-label">Current password</label>
-                            <input type="password" class="form-control">
+                            <input name="oldpass" type="password" class="form-control">
                         </div>
 
                         <div class="form-group">
                             <label class="form-label">New password</label>
-                            <input type="password" class="form-control">
+                            <input name="npass" type="password" class="form-control">
                         </div>
 
                         <div class="form-group">
                             <label class="form-label">Repeat new password</label>
-                            <input type="password" class="form-control">
+                            <input name="cpass" type="password" class="form-control">
                         </div>
 
                     </div>
                 </div>
 
                 <div class="text-right mt-3 mx-4 my-4 mb-5">
-                    <button type="button" class="btn btn-primary">Save changes</button>&nbsp;
-                    <button type="button" class="btn btn-default">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>&nbsp;
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
