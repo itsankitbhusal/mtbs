a0caeb972e17d728a0793b8fb6e80594ef80151c
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-27 19:13:20 +0545

    check booking id is provided or not

--- user/payment.inc.php
+++ user/payment.inc.php
@@ -1,17 +1,42 @@
 <?php
 require "./components/user.php";
 
-$booking = query('SELECT * FROM booking ORDER BY id DESC LIMIT 1', false);
-$booking_id = $booking['id'];
+$booking_id = request('id');
 
-$amount = $booking['total_price'];
+$booking = query('SELECT * FROM booking where id =' . $booking_id);
+
+if (empty($booking_id)) {
+    setError('Payment Failed!!');
+    header("Location: ./index.php");
+}
+// echo "<pre>";
+// print_r($booking);
+// die;
+// Array
+// (
+//     [0] => Array
+//         (
+//             [id] => 53
+//             [user_id] => 14
+//             [show_id] => 38
+//             [total_tickets] => 90
+//             [unit_price] => 500
+//             [status] => pending
+//             [booked_seat] => 1
+//             [total_price] => 500
+//             [booking_time] => 2022-03-27 18:56:47
+//         )
+
+// )
+$amount = $booking[0]['total_price'];
 
 
 $name = request('name_on_card');
 
 if (!validateName($name)) {
     setError("Please enter a valid name!!");
-    header("Location: ./payment.php");
+    $url = './payment.php?id=' . $booking_id;
+    header("Location: $url");
     die;
 }
 
@@ -20,7 +45,8 @@ $card = request('card_number');
 
 if (!validateNumber($card)) {
     setError('Please Enter valid card number!!');
-    header("Location: ./payment.php");
+    $url = './payment.php?id=' . $booking_id;
+    header("Location: $url");
     die;
 }
 
@@ -32,20 +58,22 @@ $cvv = request('cvv');
 
 if (empty($cvv)) {
     setError("Please provide all the details!!");
-    header("Location: ./payment.php");
+    $url = './payment.php?id=' . $booking_id;
+    header("Location: $url");
     die;
 }
 
 if (strlen($cvv) > 3) {
     setError("Please enter valid CVV!!");
-    header("Location: ./payment.php");
+    $url = './payment.php?id=' . $booking_id;
+    header("Location: $url");
     die;
 }
 $exists = where('payment', 'booking_id', '=', $booking_id, false);
 
 if ($exists > 5) {
     setError("You cannot Book more than 5 seats!!");
-    header("Location: ../index.php");
+    header("Location: ./index.php");
     die;
 }
 
