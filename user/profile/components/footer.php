8849b96cfd7a9f04ff72695c4d36e9654fbb4e7c
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-27 22:50:55 +0545

    added navbar

--- user/profile/components/footer.php
+++ user/profile/components/footer.php
@@ -1,4 +1,45 @@
 </div>
+</div>
+
+<!-- Footer -->
+<footer class="bg-light text-center container-fluid mt-5">
+    <!-- Grid container -->
+    <div class="container flex-wrap gap-3  p-4 d-flex justify-content-center justify-content-lg-between align-content-center">
+
+        <!-- Section: Social media -->
+        <div class="w-auto mt-4">
+
+            <img style="width: 200px;" class="img-fluid" src="../img/logoWithName.png" alt="logo">
+        </div>
+        <div class="mt-4">
+
+            <!-- Facebook -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #4267B2" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #fb3958" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #0072b1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+            <!-- Github -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #171515" href="#!" role="button"><i class="fab fa-github"></i></a>
+
+
+        </div>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center pb-5">
+        Copyright &copy; <?php echo date("Y"); ?>
+        <a class="text-muted font-weight-bold" href="#">CinemaTic</a><br>
+        Design and Developed with <span style="color:#fb3958;"><i class="fas fa-heart"></i></span> by <a class="text-muted font-weight-bold" href="https://www.facebook.com/ankitbhusal20/" target="_blank">Ankit Bhusal</a>
+    </div>
+    <!-- Copyright -->
+
+</footer> <!-- Footer -->
+
 <script src="../js/mdb.min.js"></script>
 <script src="../js/script.js"></script>
 
--- user/profile/components/header.php
+++ user/profile/components/header.php
@@ -1,3 +1,4 @@
+<?php require "./components/user.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +9,8 @@
     <title>Profile</title>
     <link rel="stylesheet" href="../css/mdb.min.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+
     <style>
         html {
             scrollbar-width: thin;
@@ -29,8 +32,78 @@
 </head>
 
 <body>
-    <div class="container light-style flex-grow-1 container-p-y my-5">
 
-        <h4 class="font-weight-bold py-3 mb-4">
-            Account settings
-        </h4>
+
+    <div>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- Container wrapper -->
+            <div class="container">
+                <!-- Toggle button -->
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+                <!-- Collapsible wrapper -->
+                <div class="collapse navbar-collapse justify-content-center " id="navbarSupportedContent">
+                    <!-- Navbar brand -->
+                    <a class="navbar-brand m-2 mt-lg-0" href="../index.php">
+                        <img src="../img/logoWithName.png" height="30" alt="MTBS Logo" loading="lazy" />
+                    </a>
+                    <!-- Left links -->
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0 font-weight-bold">
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="../../index.php">HOME</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="../../movie.php">MOVIES</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="../../about.php">ABOUT</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="../../contact.php">CONTACT</a>
+                        </li>
+                    </ul>
+                    <!-- Left links -->
+                </div>
+                <!-- Collapsible wrapper -->
+
+                <!-- Right elements -->
+                <?php if (empty(($_SESSION['user_id']))) :       ?>
+                    <div class="d-flex align-items-center mx-2">
+                        <a href="./register.php" class="btn btn-outline-primary ">Sign Up</a>
+                        <!-- Right elements -->
+                    </div>
+                    <div class="d-flex align-items-center mx-2">
+                        <a href="./login.php" class="btn btn-primary">Log In</a>
+                        <!-- Right elements -->
+                    </div>
+                <?php endif; ?>
+                <?php if (!empty(($_SESSION['user_id']))) : ?>
+
+                    <a href="../profile/" class="mx-sm-2 mx-md-4 d-flex align-items-center gap-2 p-2 rounded px-4 hover-shadow">
+                        <i class="fas fa-user-circle text-primary "></i>
+                        <p class="d-block my-auto w-responsive">
+                            <?php $user = find('user', $_SESSION['user_id']);
+                            echo substr($user['name'], 0, 6);
+                            ?>
+                        </p>
+                    </a>
+
+                    <div class="d-flex align-items-center mx-2">
+                        <a href="./logout.php" class="btn btn-danger">Log Out</a>
+                        <!-- Right elements -->
+                    </div>
+                <?php endif;    ?>
+                <!-- Container wrapper -->
+        </nav>
+        <!-- Navbar -->
+
+
+
+        <div class="container light-style flex-grow-1 container-p-y my-5">
+
+            <h4 class="font-weight-bold py-3 mb-4">
+                Account settings
+            </h4>
