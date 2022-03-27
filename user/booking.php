b4ca13124710fbe2f74c5a57d2cedf03b1c51b1e
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-27 19:12:27 +0545

    link

--- user/booking.php
+++ user/booking.php
@@ -12,6 +12,12 @@ $show_id = request('sid');
 //hall id form ticket GET method
 // $hall_id = request('hid');
 
+if (empty($show_id)) {
+    setError('Please enter valid id!!');
+    header('Location: ./index.php');
+    die;
+}
+
 //total seats
 $total_tickets = request('tt');
 
@@ -26,6 +32,17 @@ if ($booked_seat > 5) {
     die;
 }
 
+//counting tootal number of seats booked by user
+// 
+// $total_tickets_db = query("SELECT sum(booked_seat) FROM booking WHERE (user_id = $user_id)");
+// $db_seat = $total_tickets_db[0]['sum(booked_seat)'];
+
+// if (($db_seat + $booked_seat) > 5) {
+//     setError("You cannot book more than 5 seats!!");
+//     header('Location: ./index.php');
+//     die;
+// }
+
 //total price from POST
 $total_price = $unit_price * $booked_seat;
 
@@ -36,10 +53,17 @@ if (!empty($user_id) && !empty($show_id) && !empty($total_tickets) && !empty($un
     create('booking', compact('user_id', 'show_id', 'total_tickets', 'unit_price', 'status', 'booked_seat', 'total_price'));
 
     setSuccess('Make payement to confirm booking!!');
-    header("Location: ./payment.php?");
+    $booking_id = query('SELECT `id` FROM `booking` ORDER BY id DESC LIMIT 1');
+    $id = $booking_id[0]['id'];
+    // echo '<pre>';
+    // print_r($id);
+    // die;
+    $uul = './payment.php?id=' . $id;
+    header("Location: $uul");
     die;
 } else {
     setError("fill all the fields!!");
-    header("Location: ./payment.php?");
+    $url = 'ticket.php?id=' . $show_id;
+    header("Location: $url");
     die;
 }
