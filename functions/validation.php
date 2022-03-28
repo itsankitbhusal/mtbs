be85256881c374151228c475c8942245c21bb840
ankitbhusal <ankitbhusal95@gmail.com>
2022-03-28 09:50:46 +0545

    added genre validation

--- functions/validation.php
+++ functions/validation.php
@@ -50,3 +50,16 @@ function validatePassword($password)
 {
     return preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/', $password);
 }
+
+/**
+ * Validate Movie genre 
+ * matches only single word and -
+ *
+ * @param string $password string
+ *
+ * @return true
+ */
+function validateGenre($genre)
+{
+    return preg_match("/^[A-Za-z-]+$/", $genre);
+}
